d040eedd5f6b308119fb911275409f6d657809dc
abhikumar958009 <abhikumar95008@gmail.com>
2026-08-20 12:42:56 +0530

    new code updated

--- index.js
+++ index.js
@@ -100,16 +100,32 @@
 // console.log("One");
 // setTimeout(()=>console.log("Two")); 
 // console.log("Three");
-let a;
-let b;
-function sumofsqrt(a,b){
-    return Math.sqrt(a)+Math.sqrt(b);
-}
-function sumOfSqrtWithName(name,clbk){
-    const result = clbk(64,49);
-    return name+result;
-}
-const result=sumOfSqrtWithName("Abhishek Kumar...Sum=",sumofsqrt);
-console.log(result);
+// let a;
+// let b;
+// function sumofsqrt(a,b){
+//     return Math.sqrt(a)+Math.sqrt(b);
+// }
+// function sumOfSqrtWithName(name,clbk){
+//     const result = clbk(64,49);
+//     return name+result;
+// }
+// const result=sumOfSqrtWithName("Abhishek Kumar...Sum=",sumofsqrt);
+// console.log(result);
 
 
+const myPromise = new Promise((resolve, reject)=>{
+    let username = "AbhishekKumar";
+    let password = "123456"
+    if(username=="AbhishekKumar"&& password=="123456"){
+        resolve("succes");
+    }else{
+        reject("Invalid user");
+    }
+})
+myPromise.then((msg)=>{
+    console.log(msg);
+})
+.catch(msg=>{
+    console.log(msg);
+})
+.finally(()=>console.log("All resource has been closed"))
